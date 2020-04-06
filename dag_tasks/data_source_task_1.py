a5f32a93164ba86e10dbeb4c8af659f4c40f5d88
krishnagiri narra <narrakrishnagiri@gmail.com>
2020-04-06 06:51:42 -0700

    adding data source tasks

---
+++ dag_tasks/data_source_task_1.py
@@ -0,0 +1,20 @@
+# Bunch of import statements
+import os
+import shutil
+"""
+Task for data input node
+"""
+def task(filelist, pathin, pathout):
+    out_list = []
+    for f in filelist:
+        source = os.path.join(pathin, f) 
+        destination = os.path.join(pathout, f)
+        try: 
+            out_list.append(shutil.copyfile(source, destination))
+        except: 
+            print("ERROR while copying file in data_source_task.py")
+    return out_list 
+	
+if __name__ == "__main__":
+	filelist = ['n03345487_1002.JPEG']
+	task(filelist, "./datasets/fireengine/", "./to_master/")
