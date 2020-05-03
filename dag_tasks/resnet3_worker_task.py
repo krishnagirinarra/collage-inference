70485f9e3ff430aa573a9519a94d4d3ea22f4320
krishnagiri narra <narrakrishnagiri@gmail.com>
2020-05-02 21:38:22 -0700

    duplicated and created 9 resnet tasks

--- dag_tasks/resnet3_worker_task.py
+++ dag_tasks/resnet3_worker_task.py
@@ -8,7 +8,7 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import datasets
 import shutil
 
-resnet_task_num = 0
+resnet_task_num = 3
 
 def task(file_, pathin, pathout): 
     global resnet_task_num
@@ -42,12 +42,14 @@ def task(file_, pathin, pathout):
         if pred[0] == 555: ### fire engine. class 1
             source = os.path.join(pathin, f)
             f_split = f.split("prefix_")[1]
-            destination = os.path.join(pathout, "resnet" + str(resnet_task_num) + "_class1_prefix_" + f_split)
+            #destination = os.path.join(pathout, "resnet" + str(resnet_task_num) + "_class1_prefix_" + f_split)
+            destination = os.path.join(pathout, "storeclass1_" + "resnet" + str(resnet_task_num) + "_prefix_" + f_split)
             out_list.append(shutil.copyfile(source, destination))
         elif pred[0] == 779: ### school bus. class 2
             source = os.path.join(pathin, f)
             f_split = f.split("prefix_")[1]
-            destination = os.path.join(pathout, "resnet" + str(resnet_task_num) + "_class2_prefix_" + f_split)
+            #destination = os.path.join(pathout, "resnet" + str(resnet_task_num) + "_class2_prefix_" + f_split)
+            destination = os.path.join(pathout, "storeclass2_" + "resnet" + str(resnet_task_num) + "_prefix_" + f_split)
             out_list.append(shutil.copyfile(source, destination))
         else: ### not either of the classes
             source = os.path.join(pathin, f)
